038a4afdf2897f95d9876156d057069d3614b294
Yehonathan Sharvit <viebel@gmail.com>
2012-02-16 17:21:59 +0200

    introduce deftest-several to make the code of tests cleaner

--- clojure/tutorial/test/tutorial/test/list_to_xml.clj
+++ clojure/tutorial/test/tutorial/test/list_to_xml.clj
@@ -2,19 +2,24 @@
   (:use [tutorial.list-to-xml])
   (:use [clojure.test]))
 
-(deftest test-list-to-xml[]
-         (are [a b] (= a (to-xml b))
+(defmacro deftest-several [tested-func & args]
+  (let [testname (symbol (str "test-" tested-func))]
+    `(deftest ~testname [] 
+              (are [a b] (= a (~tested-func b))
+                   ~@args))))
+
+(deftest-several to-xml
+              "<gg>bb</gg>" '(:gg bb)
               "<gg>bb</gg>" '(:gg bb)
               "<gg>\n<aa>bb</aa>\n</gg>" '(:gg ((:aa bb)))
               "<gg>\n<aa>aa</aa>\n<bb>bb</bb>\n</gg>" '(:gg ((:aa aa)(:bb bb)))
               "<gg>\n<aa>aa</aa>\n<bb>bb</bb>\n<cc>\n<dd>dd</dd>\n<ee>ee</ee>\n</cc>\n</gg>" '(:gg ((:aa aa)(:bb bb)(:cc ((:dd "dd")(:ee "ee")))))
-              ))
+              )
 
-
-(deftest test-xml-attribute[]
-         (are [a b] (= a (xml-attribute? b))
+(deftest-several xml-attribute?
               false "aaa"
               true "-aa"
               false "a-a"
               false "a-"
-              ))
+              )
+
