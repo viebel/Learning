39baa546c5f7f3296620a89e0241a6599bbd01cc
Yehonathan Sharvit <viebel@gmail.com>
2012-02-23 23:48:11 +0200

    add dbg + touch file on success + fix ns issue

--- clojure/tutorial/project.clj
+++ clojure/tutorial/project.clj
@@ -1,5 +1,6 @@
 (defproject tutorial "1.7.0-SNAPSHOT"
   :description "Clojure Tutorial"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [fs "0.11.1"]
                  ]
   :main tutorial.helloworld)
--- clojure/tutorial/src/tutorial/ant.clj
+++ clojure/tutorial/src/tutorial/ant.clj
@@ -1,39 +1,44 @@
 (ns tutorial.ant
     (:use clojure.java.shell)
     (:use clojure.java.io)
+    (:require [fs])
     (:require [clojure.string :as string]))
 
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
 (defn run-target [target]
-      ((-> target symbol resolve)))
+  (-> (str "tutorial.ant/" target) symbol resolve))
 
 (defn shexec[executable files]
-      (defn print-output[output]
-            (println (string/join "\n" (reverse output))))
-      (println executable ":")
-      (loop [files files output '() errorcode 0]
-            (if (or (empty? files) (not= 0 errorcode))
-              (do (print-output output)
-               errorcode)
-              (let [file (first files) 
-                         res (sh executable file)
-                         errorcode (:exit res)
-                         msg (str file ": " (if (= 0 errorcode) "OK" (str "FAILED\n" (:err res))))]
-                  (recur (rest files) (cons msg output) errorcode)))))
+  (defn print-output[output]
+    (println (string/join "\n" output)))
+  (println executable ":")
+  (loop [files files output [] errorcode 0]
+    (if (or (empty? files) (not= 0 errorcode))
+        (do (print-output output)
+            errorcode)
+      (let [file (first files)
+                 res (sh executable file)
+                 errorcode (:exit res)
+                 msg (str file ": " (if (= 0 errorcode) "OK" (str "FAILED\n" (:err res))))]
+        (do (if (= 0 errorcode) (fs/touch (str file ".touch")))
+            (recur (rest files) (conj output msg) errorcode))))))
 
-(defn fileset 
-      ([dir] (map as-relative-path (file-seq (file dir))))
-      ([regexp-filter dir] 
-       (filter (partial re-matches regexp-filter) (fileset dir))))
+(defn fileset
+  ([dir] (map as-relative-path (file-seq (file dir))))
+  ([regexp-filter dir]
+   (filter (partial re-matches regexp-filter) (fileset dir))))
 
 (defn mobile[]
-      (shexec "xmllint" 
-              (fileset #".*\.xml$" "bb")))
+  (shexec "xmllint"
+          (fileset #".*\.xml$" "xml-ok")))
 
 (defn -main [target & args]
-      (let [res (run-target target)]
-        (if (= 0 res)
-          (println "BUILD SUCESSFUL")
-          (println "BUILD FAILED")))
-      (shutdown-agents))
+  (let [res (run-target target)]
+    (if (= 0 res)
+        (println "BUILD SUCESSFUL")
+      (println "BUILD FAILED")))
+  (shutdown-agents))
 
-(-main "mobile")
+;(-main "mobile")
