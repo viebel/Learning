c330c3cbd167a7ddbaf4ef06280010b34540c261
Yehonathan Sharvit <viebel@gmail.com>
2012-02-24 09:29:51 +0200

    do not exec on non-modified files

--- clojure/tutorial/src/tutorial/ant.clj
+++ clojure/tutorial/src/tutorial/ant.clj
@@ -8,37 +8,44 @@
 (defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 (defn run-target [target]
-  (-> (str "tutorial.ant/" target) symbol resolve))
+  ((-> (str "tutorial.ant/" target) symbol resolve)))
+
+(defn exec-and-touch[executable src target]
+      (let [res (sh executable src)]
+        (when (= 0 (:exit res)) (fs/touch target))
+        res))
 
 (defn shexec[executable files]
   (defn print-output[output]
     (println (string/join "\n" output)))
+  (defn exec[executable filename]
+    (defn newer? [a b] 
+          (apply > (map #(-> % file .lastModified) [a b])))
+    (let [target (str filename ".touch")]
+      (if (newer? target filename)
+          [0 ""]
+        (let [res (exec-and-touch executable filename (str filename ".touch"))
+                  errorcode (:exit res)]
+          [errorcode (str filename ": " (if (= 0 errorcode) "OK" (str "FAILED\n" (:err res))))]))))
+  
   (println executable ":")
   (loop [files files output [] errorcode 0]
     (if (or (empty? files) (not= 0 errorcode))
         (do (print-output output)
             errorcode)
-      (let [file (first files)
-                 res (sh executable file)
-                 errorcode (:exit res)
-                 msg (str file ": " (if (= 0 errorcode) "OK" (str "FAILED\n" (:err res))))]
-        (do (if (= 0 errorcode) (fs/touch (str file ".touch")))
-            (recur (rest files) (conj output msg) errorcode))))))
-
-(defn fileset
-  ([dir] (map as-relative-path (file-seq (file dir))))
-  ([regexp-filter dir]
-   (filter (partial re-matches regexp-filter) (fileset dir))))
+      (let [filename (first files)
+                 [errorcode msg] (exec executable filename)]
+        (recur (rest files) (conj output msg) errorcode)))))
 
 (defn mobile[]
   (shexec "xmllint"
-          (fileset #".*\.xml$" "xml-ok")))
+          (fs/glob "xml-ok/*.xml")))
 
 (defn -main [target & args]
   (let [res (run-target target)]
     (if (= 0 res)
         (println "BUILD SUCESSFUL")
-      (println "BUILD FAILED")))
+      (println (str "BUILD FAILED: " res))))
   (shutdown-agents))
 
 ;(-main "mobile")
