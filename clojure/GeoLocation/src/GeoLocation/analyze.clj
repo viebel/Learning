aa269ffd52187ae2e413cd00dcdee237d6197dd1
Yehonathan Sharvit <viebel@gmail.com>
2012-02-27 16:30:35 +0200

    stats for netspeed

--- clojure/GeoLocation/src/GeoLocation/analyze.clj
+++ clojure/GeoLocation/src/GeoLocation/analyze.clj
@@ -10,6 +10,7 @@
       (apply merge-with into m))
 
 (def lookupService (LookupService. "GeoIPISP.dat") )
+(def netspeedService (LookupService. "GeoIPNetSpeedCell.dat") )
 
 (def isps-3g (set (line-seq (reader "3g-isps.txt"))))
 
@@ -21,6 +22,20 @@
       (contains? isps-3g isp))
     (if (is-3g-isp (get-isp ip)) :cellular :wifi))
 
+(defn netspeed[ip]
+    ({11 :dialup 1 :corporate 18 :cable/dsl 28 :unknown} (.getID netspeedService ip)))
+
+(dbg (netspeed "4.0.0.0"));Dialup
+(dbg (netspeed "3.0.0.0"));corporate
+(dbg (netspeed "12.39.246.64"));Cable/DSL
+(dbg (netspeed "32.21.248.0"));Cellular
+(dbg (get-isp "32.21.248.0"));Cellular
+
+(println (every? (partial = 11) (map netspeed (line-seq (reader "dialup.txt")))))
+(println (every? (partial = 1) (map netspeed (line-seq (reader "corporate.txt")))))
+(println (every? (partial = 18) (map netspeed (line-seq (reader "cable.txt")))))
+(println (string/join "\n" (set (map get-isp (line-seq (reader "cellular.csv"))))))
+
 (defn get-device[useragent]
       (cond 
         (re-matches #".*iPhone.*" useragent) :iPhone
@@ -31,11 +46,11 @@
 (defn get-stats-per-device [predicate dbtable] 
       (defn ratio [m]
             (let [total (reduce + (vals m))]
-              (modify-vals #(float (/ % total)) m)))
+              (modify-vals #(format "%.2f" (float (/ % total))) m)))
       (let [isp-and-device (for [{:keys [ip useragent]} (ip-and-useragent-mobile dbtable)] 
                                 {(get-device useragent) [(predicate ip)]})] 
         (modify-vals (comp ratio frequencies) (aggregate-by-key isp-and-device))))
 
 (defn -main[& args]
-    #_(println (time (get-netspeed-per-device :Click26_2_12)))
-    (println (time (get-stats-per-device wifi-or-3g? :Click26_2_12))))
+    #_(println (time (get-stats-per-device netspeed :Click26_2_12)))
+    #_(println (time (get-stats-per-device wifi-or-3g? :Click26_2_12))))
