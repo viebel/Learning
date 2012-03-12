d549629b0d1ad3e73743a5737621dd47d59c95a9
Yehonathan Sharvit <viebel@gmail.com>
2012-03-12 12:24:45 +0200

    introduce utils.clj

--- clojure/GeoLocation/src/GeoLocation/Device.clj
+++ clojure/GeoLocation/src/GeoLocation/Device.clj
@@ -1,39 +1,11 @@
 (ns GeoLocation.Device
+    (:use [GeoLocation.utils])
     (:use [GeoLocation.Maxmind])
     (:require [clojure.string :as string])
     (:refer-clojure :exclude [distinct conj! case compile drop take sort disj!])
     (:use [clojureql.core])
     (:use [clojureql.predicates])
     (:import (com.maxmind.geoip LookupService)))
-
-(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
-(defn modify-vals [f m] (zipmap (keys m) (map f (vals m))))
-(defn aggregate-by-key [m] 
-      (apply merge-with into m))
-
-(defn get-device[useragent]
-      (cond 
-        (re-matches #".*iPhone.*" useragent) :iPhone
-        (re-matches #".*iPad.*" useragent) :iPad
-        (re-matches #".*Android.*" useragent) :Android
-        :else :useragent))
-
-(defn get-day-tables [prefix] 
-      (for [i (range 3 10)]
-        (keyword (str prefix i))))
-
-
-(defn csv[data c1 c2] 
-      (string/join "\n"
-                   (cons (string/join "," (list "" (name c1) (name c2)))
-                         (for [[device d] data]
-                              (string/join "," [(name device) (d c1) (d c2)])))))
-(def db
- {:classname   "com.mysql.jdbc.Driver"
- :subprotocol "mysql"
- :user        "sa"
- :password     "sakontera"
- :subname      "//10.1.5.20/Reports18_3"})
 
 (defn ips-for-useragent [useragent tablename]
       @(-> (table db tablename)
--- clojure/GeoLocation/src/GeoLocation/analyze.clj
+++ clojure/GeoLocation/src/GeoLocation/analyze.clj
@@ -1,13 +1,12 @@
 (ns GeoLocation.analyze
+    (:use [GeoLocation.utils])
     (:use [GeoLocation.Maxmind])
     (:use [clojure.java.io :only (reader)])
-    (:use [GeoLocation.sql :exclude [-main]])
+    (:refer-clojure :exclude [distinct conj! case compile drop take sort disj!])
+    (:use [clojureql.core])
+    (:use [clojureql.predicates])
     (:require [clojure.string :as string]))
 
-(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
-(defn modify-vals [f m] (zipmap (keys m) (map f (vals m))))
-(defn aggregate-by-key [m] 
-      (apply merge-with into m))
 ;(dbg (netspeed "4.0.0.0"));Dialup
 ;(dbg (netspeed "3.0.0.0"));corporate
 ;(dbg (netspeed "12.39.246.64"));Cable/DSL
@@ -21,16 +20,13 @@
 ;(println (every? (partial = 18) (map netspeed (line-seq (reader "cable.txt")))))
 ;(println (string/join "\n" (set (map get-isp (line-seq (reader "cellular.csv"))))))
 
-(defn get-device[useragent]
-      (cond 
-        (re-matches #".*iPhone.*" useragent) :iPhone
-        (re-matches #".*iPad.*" useragent) :iPad
-        (re-matches #".*Android.*" useragent) :Android
-        :else :useragent))
-
-(defn get-day-tables [prefix] 
-      (for [i (range 24)]
-        (keyword (str prefix i))))
+(defn ip-and-useragent-mobile [tablename]
+      @(-> (table db tablename)
+           (select (where (and (= :country "us") 
+                               (or (like :useragent "%Android%" )
+                                   (like :useragent "%iPad%")
+                                   (like :userAgent "%iPhone%")))))
+           (project [:ip :userAgent])))
 
 (defn get-stats-per-device [predicate table_prefix] 
       (println "gathering data for:" table_prefix)
@@ -42,12 +38,6 @@
 
       (reduce (partial merge-with (partial merge-with +))
               (map one-hour-data (get-day-tables table_prefix))))
-
-(defn csv[data c1 c2] 
-      (string/join "\n"
-                   (cons (string/join "," (list "" (name c1) (name c2)))
-                         (for [[device d] data]
-                              (string/join "," [(name device) (d c1) (d c2)])))))
 
 (defn -main
       ([] (println "Nothing to run"))
