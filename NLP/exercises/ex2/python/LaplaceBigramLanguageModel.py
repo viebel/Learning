90ffe007d44cc8455e3463046ec846bcba81c234
Yehonathan Sharvit <viebel@gmail.com>
2012-03-19 13:10:38 +0200

    LaplaceBigram

--- NLP/exercises/ex2/python/LaplaceBigramLanguageModel.py
+++ NLP/exercises/ex2/python/LaplaceBigramLanguageModel.py
@@ -1,20 +1,37 @@
-class LaplaceBigramLanguageModel:
+import math
 
+def create_couple(a,b):
+  return a + '<_>' + b
+
+class LaplaceBigramLanguageModel:
   def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.bigrams = {}
+    self.unigrams = {}
     self.train(corpus)
 
+  def num_of_occurences(self, a,b):
+      return self.bigrams.get(create_couple(a,b)) or 0
+
   def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
-        Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+    def inc(dic, item):
+        c = dic.get(item) or 0
+        dic[item] = c + 1
+        return c + 1
+
+    for sentence in corpus.corpus: # iterate over sentences in the corpus
+      previous = '<s>'
+      for datum in sentence.data[1:]: # iterate over datums in the sentence
+        inc(self.unigrams, datum.word)
+        inc(self.bigrams, create_couple(previous,datum.word))
+        previous = datum.word
+
+  def probability(self, token, previous):
+      return math.log(float(self.num_of_occurences(previous,token) + 1)/len(self.unigrams))
 
   def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
-        sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+    score = 0.0
+    previous = '<s>'
+    for token in sentence[1:]: # iterate over bigrams in the sentence
+      score += self.probability(token, previous)
+      previous = token
+    return score
