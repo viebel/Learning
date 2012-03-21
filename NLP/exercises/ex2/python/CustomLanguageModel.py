1a4cb958a9eaa17bc2f3900b84b4e258d306aab4
Yehonathan Sharvit <viebel@gmail.com>
2012-03-21 21:46:46 +0200

    interpolating

--- NLP/exercises/ex2/python/CustomLanguageModel.py
+++ NLP/exercises/ex2/python/CustomLanguageModel.py
@@ -22,7 +22,17 @@ def histogram(values_with_count):
         c = values_with_count[item]
         inc(c)
     return hist
-    
+
+def create_coeffs(n,k):
+    def inner(remainder, res, i):
+        if(i == 1):
+            res[i] = remainder
+            return res
+        res[i] = k*remainder
+        return inner((1-k)*remainder, res, i - 1)
+    return inner(1, {}, n)
+
+   
 class CustomLanguageModel:
   def __init__(self, corpus):
     self.ngram_depth = 3
@@ -32,12 +42,15 @@ class CustomLanguageModel:
     hist = histogram(self.ngrams[1])
     self.unk_probability = float(hist[1])/self.nwords
     self.unk_probability = 0.5/self.nwords
+    self.coeffs = create_coeffs(self.ngram_depth, 0.6)
     print('nwords: ' + str(self.nwords) + ' unk_probability: ' + str(self.unk_probability))
 
   def get_ngram_container(self, ngram):
     return self.ngrams[len(ngram)]
 
   def num_of_occurences(self, ngram):
+    if len(ngram) == 0:
+        return self.nwords
     return self.get_ngram_container(ngram).get(str(ngram)) or 0
 
   def train(self, corpus):
@@ -59,18 +72,35 @@ class CustomLanguageModel:
                 inc(ngram)
               i += 1
 
-  def ngram_score_raw(self, ngram):
+  def ngram_prob(self, ngram, ngram_count):
+      if ngram_count == 0:
+          return 0
+      return float(ngram_count)/self.num_of_occurences(ngram[:-1])
+
+  def ngram_score_stupid_backoff(self, ngram):
       ngram_count = self.num_of_occurences(ngram)
-      if len(ngram) == 1:
-          if ngram_count:
-              return float(ngram_count)/self.nwords
-          return self.unk_probability
       if ngram_count != 0:
-          return float(ngram_count)/self.num_of_occurences(ngram[:-1])
+          return self.ngram_prob(ngram, ngram_count)
+      if len(ngram) == 1:
+          return self.unk_probability
       return 0.4*self.ngram_score_raw(ngram[1:])
 
+  def prob_coeffs(self, ngram):
+      return self.coeffs[len(ngram)]
+
+  def ngram_score_interpolation(self, ngram, res):
+      if len(ngram) == 0:
+          return res
+      ngram_count = self.num_of_occurences(ngram)
+      k = self.prob_coeffs(ngram)
+      if len(ngram) == 1 and ngram_count == 0:
+          p = self.unk_probability
+      else:
+          p = self.ngram_prob(ngram, ngram_count)
+      return self.ngram_score_interpolation(ngram[1:], res + k*p)
+
   def ngram_score(self, ngram):
-      return math.log(self.ngram_score_raw(ngram))
+      return math.log(self.ngram_score_interpolation(ngram, 0))
 
   def score(self, sentence):
     score = 0.0
