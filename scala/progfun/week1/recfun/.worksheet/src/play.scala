d396f3a3f901e121c36e4c63e5148f724d8d9d47
Yehonathan Sharvit <yehonathan@kontera.com>
2012-11-11 20:04:18 +0200

    week 6: tmp

--- scala/progfun/week1/recfun/.worksheet/src/play.scala
+++ scala/progfun/week1/recfun/.worksheet/src/play.scala
@@ -1,23 +1,27 @@
 
 
-object play {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(35); 
+object play {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
+  lazy val inf: Stream[Int] = 1 #:: (inf map (2 * _));System.out.println("""inf  : Stream[Int] = <lazy>""");$skip(22); val res$0 = 
+  inf.take(10).toList;System.out.println("""res0: List[Int] = """ + $show(res$0));$skip(87); val res$1 = 
+  try {
+    Vector(1, 2)(8)
+  } catch {
+    case e: IndexOutOfBoundsException => 4
+  };System.out.println("""res1: Int = """ + $show(res$1));$skip(56); 
 
-  val a = (1, "a");System.out.println("""a  : (Int, java.lang.String) = """ + $show(a ));$skip(24); 
-  val (c, d) = (1, "a");System.out.println("""c  : Int = """ + $show(c ));System.out.println("""d  : java.lang.String = """ + $show(d ));$skip(38); val res$0 = 
+  def create: Int => Boolean = {
+    (x =>  false)
+  };System.out.println("""create: => Int => Boolean""");$skip(41); val res$2 = 
+ 
+ moo(4) match {
+  case moo(y) => 8
+  };System.out.println("""res2: Int = """ + $show(res$2));$skip(22); val res$3 = 
+  
+  moo(8) == moo(9);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(19); val res$4 = 
+  moo(8) == moo(8);System.out.println("""res4: Boolean = """ + $show(res$4))}
 
-  List(1, 1, 2, 3).groupBy((x) => x);System.out.println("""res0: scala.collection.immutable.Map[Int,List[Int]] = """ + $show(res$0));$skip(65); val res$1 = 
-  List(1, 1, 2, 3).groupBy((x) => x).getOrElse(19, List(9)).head;System.out.println("""res1: Int = """ + $show(res$1));$skip(32); val res$2 = 
-  (1 to 10).flatMap(List(_, 2));System.out.println("""res2: scala.collection.immutable.IndexedSeq[Int] = """ + $show(res$2));$skip(29); val res$3 = 
-
-  List(1, 2, 3).contains(2);System.out.println("""res3: Boolean = """ + $show(res$3));$skip(29); val res$4 = 
-  List((1, 2), (3, 4)).unzip;System.out.println("""res4: (List[Int], List[Int]) = """ + $show(res$4));$skip(102); 
-  def scalarProduct(x: List[Int], y: List[Int]): Int =
-    (x zip y).map { case (x, y) => x * y }.sum;System.out.println("""scalarProduct: (x: List[Int], y: List[Int])Int""");$skip(106); 
-  def scalarProduct2(x: List[Int], y: List[Int]): Int =
-    (for ((xx, yy) <- x zip y) yield xx * yy).sum;System.out.println("""scalarProduct2: (x: List[Int], y: List[Int])Int""");$skip(18); val res$5 = 
-
-List("aaa","bb");System.out.println("""res5: List[java.lang.String] = """ + $show(res$5));$skip(17); val res$6 = 
-"adsad".length();System.out.println("""res6: Int = """ + $show(res$6));$skip(13); val res$7 = 
-"123" take 1;System.out.println("""res7: String = """ + $show(res$7));$skip(13); val res$8 = 
-"123" drop 1;System.out.println("""res8: String = """ + $show(res$8))}
 }
+
+case class moo(x: Int) {
+}
+  
--- scala/progfun/week1/recfun/src/main/scala/playground/play.sc
+++ scala/progfun/week1/recfun/src/main/scala/playground/play.sc
@@ -1,27 +1,28 @@
 
 
 object play {
+  lazy val inf: Stream[Int] = 1 #:: (inf map (2 * _))
+                                                  //> inf  : Stream[Int] = <lazy>
+  inf.take(10).toList                             //> res0: List[Int] = List(1, 2, 4, 8, 16, 32, 64, 128, 256, 512)
+  try {
+    Vector(1, 2)(8)
+  } catch {
+    case e: IndexOutOfBoundsException => 4
+  }                                               //> res1: Int = 4
 
-  val a = (1, "a")                                //> a  : (Int, java.lang.String) = (1,a)
-  val (c, d) = (1, "a")                           //> c  : Int = 1
-                                                  //| d  : java.lang.String = a
+  def create: Int => Boolean = {
+    (x =>  false)
+  }                                               //> create: => Int => Boolean
+ 
+ moo(4) match {
+  case moo(y) => 8
+  }                                               //> res2: Int = 8
+  
+  moo(8) == moo(9)                                //> res3: Boolean = false
+  moo(8) == moo(8)                                //> res4: Boolean = true
 
-  List(1, 1, 2, 3).groupBy((x) => x)              //> res0: scala.collection.immutable.Map[Int,List[Int]] = Map(3 -> List(3), 1 -> 
-                                                  //| List(1, 1), 2 -> List(2))
-  List(1, 1, 2, 3).groupBy((x) => x).getOrElse(19, List(9)).head
-                                                  //> res1: Int = 9
-  (1 to 10).flatMap(List(_, 2))                   //> res2: scala.collection.immutable.IndexedSeq[Int] = Vector(1, 2, 2, 2, 3, 2, 
-                                                  //| 4, 2, 5, 2, 6, 2, 7, 2, 8, 2, 9, 2, 10, 2)
-
-  List(1, 2, 3).contains(2)                       //> res3: Boolean = true
-  List((1, 2), (3, 4)).unzip                      //> res4: (List[Int], List[Int]) = (List(1, 3),List(2, 4))
-  def scalarProduct(x: List[Int], y: List[Int]): Int =
-    (x zip y).map { case (x, y) => x * y }.sum    //> scalarProduct: (x: List[Int], y: List[Int])Int
-  def scalarProduct2(x: List[Int], y: List[Int]): Int =
-    (for ((xx, yy) <- x zip y) yield xx * yy).sum //> scalarProduct2: (x: List[Int], y: List[Int])Int
-
-List("aaa","bb")                                  //> res5: List[java.lang.String] = List(aaa, bb)
-"adsad".length()                                  //> res6: Int = 5
-"123" take 1                                      //> res7: String = 1
-"123" drop 1                                      //> res8: String = 23
 }
+
+case class moo(x: Int) {
+}
+  
